0e8dee79d7f573a43111334ce5cacb31fb1f2dea
lunamartinez01 <luna.martinez01@uptc.edu.co>
2025-08-26 20:35:25 +0000

    calculadora

---
+++ clase/MathOperation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>   // Para usar string
+using namespace std;
+
+int main() {
+    double num1, num2, resultado;
+    string operacion;
+
+    // Entrada de datos
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    cout << "Ingrese la operacion (suma, resta, multiplicacion, division): ";
+    cin >> operacion;
+
+    if (operacion == "suma") {
+        resultado = num1 + num2;
+        cout << "El resultado de la suma es: " << resultado << endl;
+    }
+    else if (operacion == "resta") {
+        resultado = num1 - num2;
+        cout << "El resultado de la resta es: " << resultado << endl;
+    }
+    else if (operacion == "multiplicacion") {
+        resultado = num1 * num2;
+        cout << "El resultado de la multiplicacion es: " << resultado << endl;
+    }
+    else if (operacion == "division") {
+        if (num2 != 0) {
+            resultado = num1 / num2;
+            cout << "El resultado de la division es: " << resultado << endl;
+        } else {
+            cout << "Error: No se puede dividir entre 0." << endl;
+        }
+    }
+    else {
+        cout << "Operacion no valida. Intente con suma, resta, multiplicacion o division22." << endl;
+    }
+
+    return 0;
+}
