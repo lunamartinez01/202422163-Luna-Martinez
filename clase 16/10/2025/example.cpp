364c5f6d735bfef9b608ddc899efa3a92c8c3a75
lunamartinez01 <luna.martinez01@uptc.edu.co>
2025-10-16 16:47:58 +0000

    clase cambiso

---
+++ clase 16/10/2025/example.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+// validar que un string sea una contraseña valida 
+//longitud minima de 8 caracteres 
+// que contenga minimo 3 caracteres especiales 
+// que contenga una mayuscula 
+//que contenga una miniscula 
+
+bool isLengthValid(string password) {
+    return password.length() >= 8;
+}
+
+bool hasSpecialCharacter(string password){
+    
+    int contador = 0;
+    for (int i = 0; i < password.size(); i++)
+    {
+        char character = password[i];
+        if (character >= '!' && character <= '/')
+        {
+            contador ++;
+        } 
+
+    }
+    if (contador >= 3)
+    {
+        return true;
+    } 
+    else 
+    {
+        return false;
+    }
+}
+
+bool validateMay (string mayus) {
+    bool containsMayus = false;
+    for (int i = 0; i < mayus.size(); i++)
+    {
+        char character = mayus[i];
+        if (character >= 'A' && character <= 'Z')
+        {
+            containsMayus = true;
+        } 
+        
+    }
+    return containsMayus;
+}
+
+void validatePassword () {
+    string password;
+    cout << "Ingrese una contraseña" << endl;
+    cin >> password;
+
+    bool lengthValid = isLengthValid(password);
+ 
+    bool hasSpecialChars = hasSpecialCharacter(password);
+    bool containsMayus = validateMay(password);
+
+    if (hasSpecialCharacter && containsMayus && lengthValid)
+    {
+        cout << "contraseña valida" << endl;
+        
+    } else {
+        cout << "contraseña invalida" << endl;
+    }
+    
+}
+
+int main(){
+    validatePassword();
+    return 0;
+}
+
+
