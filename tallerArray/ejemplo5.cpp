886c520a33be65fd901a507251f89211042bc080
lunamartinez01 <luna.martinez01@uptc.edu.co>
2025-10-30 16:57:39 +0000

    cambios finales

--- tallerArray/ejemplo5.cpp
+++ tallerArray/ejemplo5.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 void forWithArray (){
     string products[3] = {"Pan", "Leche", "Queso"};
+    float prices[3] = {1500, 3200, 4800};
+
+    for (int i = 0; i < 3; i++){
+        string arrayValue = products [i];
+        float arrayPrice = prices [i];
+        cout << "El producto: " << arrayValue << endl;
+        cout << "Tiene un valor de: " << arrayPrice << endl;
+
+    }
     
 }
 
-void float
 int main (){
     forWithArray ();
     return 0;
